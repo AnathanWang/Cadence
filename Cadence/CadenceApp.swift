da4218afe19891882c52e707f273e46e99c6bdc3
Seny <kgooe.soa@gmail.com>
2026-09-15 09:36:15 +0300

    Initial Commit

---
+++ Cadence/CadenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CadenceApp.swift
+//  Cadence
+//
+//  Created by Arseny Solyanov on 9/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CadenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cadence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cadence
+//
+//  Created by Arseny Solyanov on 9/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
